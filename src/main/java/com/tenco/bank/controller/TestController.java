96c7bd803d260d3582fce882c9401cc969d88b88
Jincchus <chopoo2001@gmail.com>
2024-01-26 14:04:51 +0900

    exception handler 기본 코드 완료

--- src/main/java/com/tenco/bank/controller/TestController.java
+++ src/main/java/com/tenco/bank/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tenco.bank.handler.exception.CustomPageException;
+import com.tenco.bank.handler.exception.CustomRestfulException;
 
 
 
@@ -14,7 +18,8 @@ public class TestController {
 	// 주소설계
 	// http://localhost:80/test/main	
 	@GetMapping("/main")
-	public String mainPage() {
+	public void mainPage() {
+		
 		// 인증 검사
 		// 유효성 검사
 		// 뷰 리졸브 -> 해당 하는 파일 찾아 (data)
@@ -22,7 +27,11 @@ public class TestController {
 		// prefix: /WEB-INF/view/
 		// layout/main
 		// suffix: .jsp
-		return "layout/main";
+		
+		// 예외 발생
+		throw new CustomRestfulException("페이지가 없습니다!!!", HttpStatus.NOT_FOUND);
+		
+//		return "layout/main";
 	}
 	
 
