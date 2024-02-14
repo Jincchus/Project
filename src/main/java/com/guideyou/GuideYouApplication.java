419c0065646c21971100051dbec049ed6842a64d
Jincchus <chopoo2001@gmail.com>
2024-02-14 12:07:58 +0900

    first commit

---
+++ src/main/java/com/guideyou/GuideYouApplication.java
@@ -0,0 +1,13 @@
+package com.guideyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideYouApplication.class, args);
+	}
+
+}
