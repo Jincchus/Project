2727bfba4cc81e79e788e72e12272400a2b2e7c3
cwal8202 <cwal8202@naver.com>
2024-02-15 16:02:07 +0900

    test - 최장호

--- src/main/java/com/guideyou/GuideYouApplication.java
+++ src/main/java/com/guideyou/GuideYouApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GuideYouApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GuideYouApplication.class, args);
+		System.out.println("최장호");
 	}
 
 }
